c1825af0b2a344a811bfbb69b60916328716c379
xlk <1148038404@qq.com>
2024-03-14 14:56:21 +0800

    feat: 简单的发布订阅模型

--- src/index.js
+++ src/index.js
@@ -1,21 +1,67 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-function App() {
-  const [count, setCount] = useState(0);
+class Chat {
+  constructor() {
+    this.list = {};
+  }
+  subscribeMessage(id, callback) {
+    const callbackList = this.list[id] || [];
+    callbackList.push(callback);
+    this.list[id] = callbackList;
+  }
 
+  unSubscribeMessage(id, callback) {
+    const index = this.list[id].indexOf(callback);
+    this.list[id].splice(index, 1);
+  }
+
+  publish(status) {
+    Object.keys(this.list).forEach((id) => {
+      this.list[id].forEach((callback) => {
+        callback(status);
+      });
+    });
+  }
+}
+
+const ChatApi = new Chat();
+
+setTimeout(() => {
+  ChatApi.publish("offline");
+}, 2000);
+
+function FriendStatus(props) {
+  const [status, setStatus] = useState(null);
+
+  function handleStatus(status) {
+    console.log("handleStatus");
+    setStatus(status);
+  }
+  useEffect(() => {
+    console.log("subscribeMessage");
+    ChatApi.subscribeMessage(props.id, handleStatus);
+    return () => {
+      console.log("unSubscribeMessage");
+      ChatApi.unSubscribeMessage(props.id, handleStatus);
+    };
+  });
+
+  if (status == null) {
+    return <h1>loading....</h1>;
+  }
+
+  return <h1>{status}</h1>;
+}
+
+function App() {
   return (
     <div>
-      <div>this count is {count}</div>
-      <button onClick={() => setCount(count + 1)}>点我</button>
+      <FriendStatus id="1" />
     </div>
   );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
